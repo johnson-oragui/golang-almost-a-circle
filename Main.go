18a1029601eecf72db9374df5c1233fb32722944
johnson-oragui <johnson-oragui@gmail.com>
2024-09-17 11:32:21 +0100

    created main entry point

---
+++ Main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/johnson-oragui/golang-almost-a-circle/models"
+)
+
+
+func main() {
+
+	b1 := models.NewBase()
+
+	b2 := models.NewBase()
+
+	b3 := models.NewBase()
+
+	b4 := models.NewBase(12)
+
+	b5 := models.NewBase()
+
+	fmt.Printf("%d\n%d\n%d\n%d\n%d\n", b1.Id, b2.Id, b3.Id, b4.Id, b5.Id)
+}

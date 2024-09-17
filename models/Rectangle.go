d5d1455398b16c264cfe8362534b95bae52273fb
johnson-oragui <johnson-oragui@gmail.com>
2024-09-17 13:39:36 +0100

    set the struct fields as exportable

--- models/Rectangle.go
+++ models/Rectangle.go
@@ -4,32 +4,32 @@ package models
 Struct Rectangle:
 
 Define a struct named Rectangle that embeds the Base struct.
-Define public fields: width, height, x, and y of type int.
+Define public fields: Width, Height, x, and y of type int.
 NewRectangle function:
 
 Implement a function named NewRectangle that takes a variable number of int arguments.
 Inside the function:
 Create a new instance of the Rectangle struct using &Rectangle{}.
-Extract values for width, height, x, and y from the variadic arguments.
+Extract values for Width, Height, x, and y from the variadic arguments.
 Create a new Base instance using NewBase and assign it to the embedded Base field of the Rectangle instance.
 Return a pointer to the newly created Rectangle struct.
 */
 
 type Rectangle struct {
-	width int
-	height int
-	x int
-	y int
+	Width int
+	Height int
+	X int
+	Y int
 	Base
 }
 
 
 func NewRectangle(values...int) *Rectangle {
 	rectangle := &Rectangle{
-		width: 0,
-		height: 0,
-		x: 0,
-		y: 0,
+		Width: 0,
+		Height: 0,
+		X: 0,
+		Y: 0,
 	}
 
 	switch len(values) {
@@ -37,24 +37,24 @@ func NewRectangle(values...int) *Rectangle {
 		rectangle.Base = *NewBase()
 		return rectangle
 	case 1:
-		rectangle.width = values[0]
+		rectangle.Width = values[0]
 	case 2:
-		rectangle.width = values[0]
-		rectangle.height = values[1]
+		rectangle.Width = values[0]
+		rectangle.Height = values[1]
 	case 3:
-		rectangle.width = values[0]
-		rectangle.height = values[1]
-		rectangle.x = values[2]
+		rectangle.Width = values[0]
+		rectangle.Height = values[1]
+		rectangle.X = values[2]
 	case 4:
-		rectangle.width = values[0]
-		rectangle.height = values[1]
-		rectangle.x = values[2]
-		rectangle.y = values[3]
+		rectangle.Width = values[0]
+		rectangle.Height = values[1]
+		rectangle.X = values[2]
+		rectangle.Y = values[3]
 	default:
-		rectangle.width = values[0]
-		rectangle.height = values[1]
-		rectangle.x = values[2]
-		rectangle.y = values[3]
+		rectangle.Width = values[0]
+		rectangle.Height = values[1]
+		rectangle.X = values[2]
+		rectangle.Y = values[3]
 		rectangle.Base = *NewBase(values[4])
 		return rectangle
 	}

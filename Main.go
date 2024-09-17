86ac89f31ad6153c54cc0f3211b0d02c1a5247ab
johnson-oragui <johnson-oragui@gmail.com>
2024-09-17 13:10:30 +0100

    created rectangle structure

--- Main.go
+++ Main.go
@@ -20,7 +20,25 @@ func TaskZeroMain() {
 	fmt.Printf("%d\n%d\n%d\n%d\n%d\n", b1.Id, b2.Id, b3.Id, b4.Id, b5.Id)
 }
 
+func TaskOneMain() {
+	r1 := models.NewRectangle(10, 2)
+
+	fmt.Println(r1.Id)
+	r2 := models.NewRectangle(2, 10)
+
+	fmt.Println(r2.Id)
+
+	r3 := models.NewRectangle(10, 2, 0, 0, 12)
+
+	fmt.Println(r3.Id)
+
+	r4 := models.NewRectangle()
+
+	fmt.Println(r4.Id)
+}
+
 func main() {
-	TaskZeroMain()
+	// TaskZeroMain()
+	TaskOneMain()
 	
 }
